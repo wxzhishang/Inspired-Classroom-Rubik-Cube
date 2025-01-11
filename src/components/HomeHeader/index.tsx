81c7a12d107bd10e0cc1b16dc446517b1a6eaaaf
daisy <2358648339@qq.com>
2025-01-11 21:21:27 +0800

    feat：注册样式

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,7 +1,16 @@
+/*
+ * @Author: daisy 2358648339@qq.com
+ * @Date: 2025-01-10 12:16:22
+ * @LastEditors: daisy 2358648339@qq.com
+ * @LastEditTime: 2025-01-11 14:34:50
+ * @FilePath: \Inspired-Classroom-Rubik-Cube\src\components\HomeHeader\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import './index.css'
 import { FlagTwoTone, UserOutlined } from '@ant-design/icons';
 import { Avatar, Dropdown } from 'antd';
 import { MenuItem } from './types'
+import { Link } from 'react-router-dom';
 
 const HomeHeader = () => {
     const items: MenuItem[] = [
@@ -31,7 +40,12 @@ const HomeHeader = () => {
             <div className="header-nav"></div>
             <div className="header-avatar">
                 <Dropdown menu={{ items }}>
-                    <Avatar size="large" icon={<UserOutlined />} style={{ float: "right", marginRight: 30, marginTop: 10 }} />
+                    <Link to={'/register'} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <Avatar
+                            size="large" icon={<UserOutlined />}
+                            style={{ float: "right", marginRight: 30, marginTop: 10 }}
+                        />
+                    </Link>
                 </Dropdown>
             </div>
         </div>
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,8 +1,73 @@
+import type { FormProps } from 'antd';
+import { Button, Form, Input, message } from 'antd';
+import './index.css'
+
+type FieldType = {
+    username?: string;
+    password?: string;
+    avatar?: string;
+    email?: string;
+    phone?: string;
+};
+
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+    message.error('注册失败！');
+};
 
 const Register = () => {
+    const [messageApi, contextHolder] = message.useMessage();
     return (
-        <div>
-            注册
+        <div className="register-main">
+            {contextHolder}
+            <div className="register-form">
+                <h2>注册新用户</h2>
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 12 }}
+                    style={{ maxWidth: 500 }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item<FieldType>
+                        label="用户名"
+                        name="username"
+                        rules={[{ required: true, message: '用户名不能为空！' }]}
+                    >
+                        <Input style={{ backgroundColor: 'transparent', border: '1px, solid, #ccc' }} />
+                    </Form.Item>
+                    <Form.Item<FieldType>
+                        label="密码"
+                        name="password"
+                        rules={[{ required: true, message: '密码不能为空！' }]}
+                    >
+                        <Input.Password style={{ backgroundColor: 'transparent', border: '1px, solid, #ccc' }} />
+                    </Form.Item>
+                    <Form.Item<FieldType>
+                        label="邮箱"
+                        name="email"
+                    >
+                        <Input style={{ backgroundColor: 'transparent', border: '1px, solid, #ccc' }} />
+                    </Form.Item>
+                    <Form.Item<FieldType>
+                        label="手机号"
+                        name="phone"
+                    >
+                        <Input style={{ backgroundColor: 'transparent', border: '1px, solid, #ccc' }} />
+                    </Form.Item>
+                    <Form.Item label={null}>
+                        <Button type="primary" htmlType="submit">
+                            注册
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </div>
     );
 }
